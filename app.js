877ac1646f122863c44e9ca1753b81b59d268dad
Shabari Nathan <83392438+Shabari02@users.noreply.github.com>
2022-08-07 20:35:05 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+const countDownDate = new Date("Aug 07, 2023 00:00:00 ").getTime();
+
+var myfunc = setInterval(function () {
+  var now = new Date().getTime();
+  var timeleft = countDownDate - now;
+  var days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("mins").innerHTML = minutes;
+  document.getElementById("secs").innerHTML = seconds;
+}, 1000);
